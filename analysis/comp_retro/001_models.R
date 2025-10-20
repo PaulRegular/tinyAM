193865d08dee761fc340baf805013f72062ddd35
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2025-10-20 14:39:26 -0230

    Exploring use of year_blocks to aid convergence of different model structures

--- analysis/comp_retro/001_models.R
+++ analysis/comp_retro/001_models.R
@@ -8,46 +8,101 @@ library(plotly)
 
 ## Terminal fit ----
 
+## More age blocks appear to work better than fewer
 cod_obs <- tinyAM::cod_obs
 cod_obs$catch$F_a_block <- cut_ages(cod_obs$catch$age, c(0:14))
+cod_obs$weight$collapse <- ifelse(cod_obs$weight$year %in% 1991:1994, 1, 0)
+
+## Couple F and M processes across the first few years to stabilize
+## early estimates and reduce confounding with initial abundance
+year_breaks <- c(1983, 1986:2024)
 
 sam_style1 <- fit_tam(
   cod_obs,
   years = 1983:2024,
   ages = 2:14,
-  N_settings = list(process = "iid", init_N0 = FALSE),
-  F_settings = list(process = "approx_rw", mu_form = NULL, mean_ages = 5:14),
-  M_settings = list(process = "off", assumption = ~I(0.3), mean_ages = 5:14),
-  obs_settings = list(q_form = ~ q_block, sd_form = ~ sd_obs_block, fill_missing = TRUE),
-  proj_settings = list(n_proj = 5, n_mean = 3, F_mult = 1)
+  N_settings = list(
+    process = "iid",
+    init_N0 = FALSE
+  ),
+  F_settings = list(
+    process = "approx_rw",
+    mu_form = NULL,
+    mean_ages = 5:14,
+    year_breaks = year_breaks
+  ),
+  M_settings = list(
+    process = "off",
+    assumption = ~I(0.3),
+    mean_ages = 5:14,
+    year_breaks = year_breaks
+  ),
+  obs_settings = list(
+    q_form = ~ q_block,
+    sd_form = ~ sd_obs_block,
+    fill_missing = TRUE
+  ),
+  proj_settings = list(
+    n_proj = 5,
+    n_mean = 3,
+    F_mult = 1
+  )
 )
 sam_style1$sdrep
-sam_style1$opt$objective # 989.6083
+sam_style1$opt$objective
 
-sam_style2 <- update(sam_style1,
-                     N_settings = list(process = "iid", init_N0 = FALSE),
-                     F_settings = list(process = "ar1", mu_form = ~F_a_block, mean_ages = 5:14),
-                     M_settings = list(process = "off", assumption = ~I(0.3), mean_ages = 5:14))
+sam_style2 <- update(
+  sam_style1,
+  F_settings = list(
+    process = "ar1",
+    mu_form = ~F_a_block,
+    mean_ages = 5:14,
+    year_breaks = year_breaks
+  )
+)
 sam_style2$sdrep
 
-ncam_style1 <- update(sam_style1,
-                      N_settings = list(process = "off", init_N0 = TRUE),
-                      F_settings = list(process = "approx_rw", mu_form = NULL, mean_ages = 5:14),
-                      M_settings = list(process = "ar1",
-                                        mu_form = NULL,
-                                        assumption = ~M_assumption,
-                                        age_breaks = c(2:9, 14),
-                                        mean_ages = 5:14))
+ncam_style1 <- update(
+  sam_style1,
+  N_settings = list(
+    process = "off",
+    init_N0 = TRUE
+  ),
+  F_settings = list(
+    process = "approx_rw",
+    mu_form = NULL,
+    mean_ages = 5:14,
+    year_breaks = year_breaks
+  ),
+  M_settings = list(
+    process = "ar1",
+    mu_form = ~ 0 + collapse,
+    assumption = ~I(0.3),
+    age_breaks = c(2:8, 14),
+    mean_ages = 5:14,
+    year_breaks = year_breaks
+  )
+)
 ncam_style1$sdrep
-ncam_style1$opt$objective # 811.0894
+ncam_style1$opt$objective
 
-ncam_style2 <- update(ncam_style1,
-                      F_settings = list(process = "approx_rw", mu_form = ~F_a_block, mean_ages = 5:14),
-                      M_settings = list(process = "ar1",
-                                        mu_form = NULL,
-                                        assumption = ~I(0.3),
-                                        age_breaks = c(2, 14),
-                                        mean_ages = 5:14))
+ncam_style2 <- update(
+  ncam_style1,
+  F_settings = list(
+    process = "approx_rw",
+    mu_form = NULL,
+    mean_ages = 5:14,
+    year_breaks = year_breaks
+  ),
+  M_settings = list(
+    process = "approx_rw",
+    mu_form = NULL,
+    assumption = ~I(0.3),
+    age_breaks = c(2, 14),
+    mean_ages = 5:14,
+    year_breaks = year_breaks
+  )
+)
 ncam_style2$sdrep
 
 
@@ -60,8 +115,6 @@ vis_tam(
   ),
   output_file = "analysis/comp_retro/001_models.html"
 )
-
-## TODO: Std. Error estimates for mu_f are always large. Consider a fix.
 
 
 
