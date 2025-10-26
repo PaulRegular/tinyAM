086afc1c3bd729ef342f51d644d22c85c4cb089d
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2025-10-25 23:46:22 -0230

    Replace log_sd_obs in global vars

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@ utils::globalVariables(c(
   "year", "age", "is_proj",
   # captured-from-dat/pars in nll_fun / make_par
   "years", "ages", "proj_settings", "N_settings", "F_settings", "M_settings",
-  "obs_settings",  "log_sd_r", "log_sd_f", "log_sd_n", "log_sd_m", "log_sd_obs",
+  "obs_settings",  "log_sd_r", "log_sd_f", "log_sd_n", "log_sd_m", "log_sd_catch", "log_sd_index",
   "logit_phi_n", "logit_phi_f", "logit_phi_m",
   "log_r0", "log_mu_f", "log_mu_m", "log_mu_assumed_m", "log_q",
   "F_modmat", "M_modmat", "q_modmat", "sd_catch_modmat", "sd_index_modmat", "obs_map",
