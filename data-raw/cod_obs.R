10388ffee9828528f30548eb3a8f7fc85115ff97
Paul Regular <Paul.Regular@dfo-mpo.gc.ca>
2026-09-25 12:07:53 -0230

    Update Northern cod data

--- data-raw/cod_obs.R
+++ data-raw/cod_obs.R
@@ -18,6 +18,7 @@ names(index)[names(index) == "index"] <- "obs"
 index$survey <- "Fall 2J3KL"
 index$samp_time <- 0.8
 index$q_block <- cut(index$age, c(min(index$age):6, max(index$age) + 1), right = FALSE)
+index$obs <- index$obs * 12048556/1000 # use number of trawl units to bump up to total abundance
 
 weight <- inputs$pwts[, c("year", "age", "weight")]
 names(weight)[names(weight) == "weight"] <- "obs"
@@ -27,10 +28,27 @@ maturity <- inputs$mats[inputs$mats$year <= max(catch$year),
 names(maturity)[names(maturity) == "mat"] <- "obs"
 
 nm <- as.data.frame.table(inputs$nm, stringsAsFactors = FALSE)
-names(nm) <- c("age", "year", "M_assumption")
+names(nm) <- c("age", "year", "NCAM_M_assumption")
 nm$age <- as.numeric(nm$age)
 nm$year <- as.numeric(nm$year)
 weight <- merge(weight, nm, by = c("year", "age"))
+
+## Allometric M: sensu Cadigan, Rideout & Regular (2022), NAFO SCR 22/039
+## Ocean-fish weight exponent from Lorenzen (1996).
+allometric_b <- -0.305
+reference_M <- 0.30
+reference_ages <- 5:14
+
+i_ref <- weight$age %in% reference_ages &
+  is.finite(weight$obs) &
+  weight$obs > 0
+
+M0 <- reference_M /
+  mean(weight$obs[i_ref] ^ allometric_b)
+
+weight$allometric_M_assumption <-
+  M0 * weight$obs ^ allometric_b
+plot(weight$age, weight$allometric_M_assumption, log = "y", xlab = "age", ylab = "Allometric M")
 
 cod_obs <- list(catch = catch,
                 index = index,
